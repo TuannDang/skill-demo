621c3a0eb009a84a66e32f1d18ddd99faa3196b8
TuannDang <37556727+TuannDang@users.noreply.github.com>
2022-05-01 14:48:07 -0700

    Create SkillDemoTester.java

---
+++ SkillDemoTester.java
@@ -0,0 +1,11 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class SkillDemoTester{
+  
+  @Test
+  public void testing (){
+     assertEquals(4,SkillDemo.squareInt(4)); //Failing test
+  }
+  
+}
